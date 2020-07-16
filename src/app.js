b0d05bc53763f3e447fbada03cf02a3f46143c93
Vishnu Ghosh <vishnugh@gmail.com>
2020-07-16 23:32:03 +0530

    Initial setup: React, Babel and Webpack

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <h1>My Website</h1>
+
+ReactDOM.render(template, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const outputPath = path.join(__dirname, 'public');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: outputPath,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+}
