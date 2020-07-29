2b73914958bad8767e92cb2dbd53cd2bf510beca
Vishnu Ghosh <vishnugh@gmail.com>
2020-07-30 02:00:31 +0530

    Webpack devtools + devServer

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,9 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/
         }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: outputPath
     }
 }
